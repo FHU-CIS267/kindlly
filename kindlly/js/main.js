98c5b2ebb77eb9a8669692c42a24834ab4de01c9
Cody-Todd <60750987+Cody-Todd@users.noreply.github.com>
2020-10-25 14:35:27 -0500

    Fetch working properly

--- kindlly/js/main.js
+++ kindlly/js/main.js
@@ -11,5 +11,17 @@ var app = new Vue({
         message: function(){
             return `Hello, ${this.user.username}`;
         }
+    },
+
+    methods: {
+        loadData: async function () {
+            let response = await fetch("kindllys.json");
+            let json = await response.json();
+            console.log(json);
+        }
+    },
+
+    created: function () {
+        this.loadData();
     }
 });
