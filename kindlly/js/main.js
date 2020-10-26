16afcbd904aa2943b285b1c1cc5e2494df323f43
DanielRoss1 <70766809+DanielRoss1@users.noreply.github.com>
2020-10-26 16:27:57 -0500

    Discover Kindllys, My Kindllys, finished.

--- kindlly/js/main.js
+++ kindlly/js/main.js
@@ -1,15 +1,30 @@
 var app = new Vue({
     el: '#app',
     data: {
-        user: {
-            username: "kenancasey",
-            avatar: "img/avatars/male-square.png"
+        discoverKindllys: [],
+        myKindllys: [],
+        user: {}
+    },
+    methods: {
+        getStuff: async function() {
+            let response = await fetch("kindllys.json");
+            let json = await response.json();
+            this.discoverKindllys = json.discoverKindllys;
+            this.myKindllys = json.myKindllys;
+            this.user = json.currentUser;
+            console.log(json);
+        },
+
+        addKindllytoStuff: function(kindlly, kindllys) {
+            kindllys.splice(kindllys.indexOf(kindlly), 1);
+            let selectedKindlly = kindlly;
+            selectedKindlly.isCompleted = false;
+            selectedKindlly.dateAdded = new Date();
+            this.myKindllys.push(selectedKindlly);
         }
     },
-    computed:
-    {
-        message: function(){
-            return `Hello, ${this.user.username}`;
-        }
+
+    created: function() {
+        this.getStuff();
     }
 });
